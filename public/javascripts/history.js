72fea3d8e26ab4ef0746e8cd5b4dfada85ba791b
Joakim Antman <antmanj@gmail.com>
2015-07-15 23:14:23 +0300

    Play icons

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -4,48 +4,54 @@ $(function() {
   var end = Math.round(new Date().getTime() / 1000);
   var start = Math.round(new Date(2014, 08, 1).getTime() / 1000);
   var step = 2500;
-  
+
   var playStep = 6000;
-  
+
   var nImages = 0;
-  
+
   var elements = $('[data-stamp-url]');
   var imgs = [];
-  
+
   function setImages() {
     $(imgs).each(function(){
       this.element.src = this.image.src;
     });
   }
-  
+
   function changeImage(stamp) {
     nImages = imgs.length;
-    
+    updateDate(stamp);
+
     $(imgs).each(function (){
       this.image.src = $(this.element).data('stamp-url') + '/' + stamp;
     });
   }
-  
+
+  function updateDate(stamp){
+    $('.current-timestamp').text(new Date(stamp*1000) + " StepSize: " + playStep);
+  }
+
   elements.each(function(){
    var img = new Image();
-   
-   img.onload = function() {
+
+   img.onerror = img.onload = function() {
     nImages--;
     if(nImages === 0) {
       setImages();
     }
   };
-  
+
+
   imgs.push({
-    element: this, 
+    element: this,
     image :img
    });
  });
- 
+
   $('#slider').attr('min', start).attr('max', end).attr('step', step).rangeslider({
     polyfill:false,
     onSlide: function(position, value) {
-        $('.current-timestamp').text(new Date(value*1000));
+        updateDate(value);
     },
     onSlideEnd: function(position, value) {
       changeImage(value);
@@ -53,16 +59,24 @@ $(function() {
   }).change(function() {
     changeImage($(this).val());
   });
-  
+
   var interval;
-  
+
+  $('#fb').click(function() {
+    playStep -= 1000;
+  });
+
+  $('#ff').click(function() {
+    playStep += 1000;
+  });
+
   $('#play').click(function() {
     if(interval) {
       clearInterval(interval);
       interval = null;
-      $(this).text('PLAY');
+      $(this).removeClass('glyphicon-pause');
     } else {
-      $(this).text('STOP');
+      $(this).addClass('glyphicon-pause');
       interval = setInterval(function() {
         if(nImages === 0) {
           var newStep = parseInt($("#slider").val(), 10) + playStep;

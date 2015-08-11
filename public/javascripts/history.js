a1a8f1643b9449e930b213e7df878cd20548b787
Joakim Antman <antmanj@gmail.com>
2015-08-11 22:46:14 +0300

    Different sliders for date and time

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -1,99 +1,132 @@
-$(function() {
-  var transparent = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-  var playStep = 60 * 60;
+/*global jQuery:true*/
+(function($) {
+  'use strict';
+  $(function() {
+    var transparent = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    var playStep = 60 * 60;
 
-  var nImages = 0;
+    var nImages = 0;
 
-  var elements = $('[data-stamp-url]');
-  var imgs = [];
+    var elements = $('[data-stamp-url]');
+    var imgs = [];
 
-  function setImages() {
-    $(imgs).each(function() {
-      this.element.src = this.image.src;
-    });
-  }
+    var dateSlider = $('#dateSlider');
+    var timeSlider = $('#timeSlider');
 
-  function changeImage(stamp) {
-    nImages = imgs.length;
-    updateDate(stamp);
-    $(imgs).each(function() {
-      this.image.src = $(this.element).data('stamp-url') + '/' + stamp;
-    });
-  }
+    function setCurrentTimestamp(stamp) {
+      var currentDate = new Date(stamp*1000);
+      var currentTime = currentDate.getHours() * 60 * 60 + currentDate.getMinutes() * 60 + currentDate.getSeconds();
 
-  function updateDate(stamp) {
-    $('.current-timestamp').text(new Date(stamp * 1000) + " StepSize: " + (playStep / 60) + "min");
-  }
+      currentDate.setHours(0);
+      currentDate.setMinutes(0);
+      currentDate.setSeconds(0);
 
-  elements.each(function() {
-    var img = new Image();
+      dateSlider.val(currentDate.getTime()/ 1000 / 60 / 60 / 24);
+      timeSlider.val(currentTime);
 
-    img.onerror = function() {
-      this.src = transparent;
+      $([dateSlider, timeSlider]).rangeslider('update', true);
     }
 
-    img.onload = function() {
-      nImages--;
-      if (nImages === 0) {
-        setImages();
+    function getCurrentTimestamp() {
+      var date = parseFloat(dateSlider.val(), 10);
+      var time = parseInt(timeSlider.val(), 10);
+
+      var stamp = date * 60 * 60 * 24;
+
+      return stamp + time;
+    }
+
+    function setImages() {
+      $(imgs).each(function() {
+        this.element.src = this.image.src;
+      });
+    }
+
+    function updateImages(noLoad) {
+      nImages = imgs.length;
+      var stamp = getCurrentTimestamp();
+
+      updateDate(stamp);
+      if (noLoad !== true) {
+        $(imgs).each(function() {
+          this.image.src = $(this.element).data('stamp-url') + '/' + stamp;
+        });
       }
-    };
+    }
 
-    imgs.push({
-      element: this,
-      image: img
+    function updateDate(stamp) {
+      $('.current-timestamp').text(new Date(stamp * 1000) + " StepSize: " + (playStep / 60) + "min");
+    }
+
+    elements.each(function() {
+      var img = new Image();
+
+      img.onerror = function() {
+        this.src = transparent;
+      };
+
+      img.onload = function() {
+        nImages--;
+        if (nImages === 0) {
+          setImages();
+        }
+      };
+
+      imgs.push({
+        element: this,
+        image: img
+      });
+    });
+
+    $('input[type="range"]').rangeslider({
+      polyfill: false,
+      onSlide: function(position, value) {
+        updateImages(true);
+      },
+      onSlideEnd: function(position, value) {
+        updateImages();
+      }
+    }).change(function() {
+      updateImages();
+    }).change();
+
+    var interval;
+
+    $('#fb').click(function(ev) {
+      ev.preventDefault();
+      playStep -= 60 * 60 * 1;
+    });
+
+    $('#ff').click(function(ev) {
+      ev.preventDefault();
+      playStep += 60 * 60 * 1;
+    });
+
+    $('#play').click(function(ev) {
+      ev.preventDefault();
+      if (interval) {
+        clearInterval(interval);
+        interval = null;
+        $(this).removeClass('glyphicon-pause');
+      } else {
+        $(this).addClass('glyphicon-pause');
+
+        interval = setInterval(function() {
+          if (nImages === 0) {
+            var newStep = getCurrentTimestamp() + playStep;
+
+            if ($('#skip-nights').is(":checked")) {
+              var hours = new Date(newStep * 1000).getHours();
+              do {
+                hours = new Date(newStep * 1000).getHours();
+                newStep += playStep;
+              } while (hours >= 22 || hours <= 5);
+            }
+            setCurrentTimestamp(newStep);
+            updateImages();
+          }
+        }, 500);
+      }
     });
   });
-
-  $('#slider').rangeslider({
-    polyfill: false,
-    onSlide: function(position, value) {
-      updateDate(value);
-    },
-    onSlideEnd: function(position, value) {
-      changeImage(value);
-    }
-  }).change(function() {
-    changeImage($(this).val());
-  }).change();
-
-  var interval;
-
-  $('#fb').click(function(ev) {
-    ev.preventDefault();
-    playStep -= 60 * 60 * 1;
-  });
-
-  $('#ff').click(function(ev) {
-    ev.preventDefault();
-    playStep += 60 * 60 * 1;
-  });
-
-  $('#play').click(function(ev) {
-    ev.preventDefault();
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
-      $(this).removeClass('glyphicon-pause');
-    } else {
-      $(this).addClass('glyphicon-pause');
-
-      interval = setInterval(function() {
-        if (nImages === 0) {
-          var newStep = parseInt($("#slider").val(), 10) + playStep;
-
-          if ($('#skip-nights').is(":checked")) {
-            do {
-              var hours = new Date(newStep * 1000).getHours();
-              newStep += playStep;
-            } while (hours >= 22 || hours <= 6)
-          }
-
-          $("#slider").val(newStep).change();
-          $('input[type="range"]').rangeslider('update', true);
-        }
-
-      }, 500);
-    }
-  });
-});
+}(jQuery));
--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,28 @@ var repo = fileRepo({
 /* GET home page. */
 router.get('/', function(req, res, next) {
   repo.getMetaData(function(metadata) {
+
+    var minDate = new Date(metadata.minCreated*1000);
+    var maxDate = new Date(metadata.maxCreated*1000);
+    var maxTime = maxDate.getHours() * 60 * 60 + maxDate.getMinutes() * 60 + maxDate.getSeconds();
+
+    maxDate.setHours(0);
+    maxDate.setMinutes(0);
+    maxDate.setSeconds(0);
+
+    minDate.setHours(0);
+    minDate.setMinutes(0);
+    minDate.setSeconds(0);
+
     var viewModel = {
       title: 'History',
       tags: [],
-      minDate: metadata.minCreated,
-      maxDate: metadata.maxCreated
+      minDate: minDate.getTime() / 1000 / 60 / 60 / 24,
+      maxDate: maxDate.getTime() / 1000 / 60 / 60 / 24,
+      currentTime: maxTime,
+      currentDate: maxDate.getTime() / 1000 / 60 / 60 / 24
     };
+
     for (var property in conf.files) {
       viewModel.tags.push(property);
     }
